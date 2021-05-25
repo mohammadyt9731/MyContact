d2542b178d1a1cb19ef179038b9c20ea46f55a41
Mohammad Yazdi <mohammadyt1379@gmail.com>
2021-05-25 14:14:52 +0430

    add comment

--- app/src/main/java/com/ttp/mycontact/MainActivity.java
+++ app/src/main/java/com/ttp/mycontact/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    //
     RecyclerView recyclerViewContacts;
     FloatingActionButton floatingBtnAddContact;
     List<Contact> contactsList;
